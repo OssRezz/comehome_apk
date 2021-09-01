f8c0179c5010415de05788f39d5d30a14f13bb96
OssRezz <OssRezz.13@gmail.com>
2021-08-31 20:35:38 -0500

    add_features_to_Sede&Users

--- src/dao/SedeDao.java
+++ src/dao/SedeDao.java
@@ -1,4 +1,3 @@
-
 package dao;
 
 import CONEXION.Conexion;
@@ -9,7 +8,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class SedeDao {
 
@@ -68,4 +66,70 @@ public class SedeDao {
         }
         return null;
     }
+
+    //Lista de sedes por id
+    public List<SedeDto> listarSedesById(int sede) {
+
+        List<SedeDto> listaDeUnaSede = new ArrayList<>();
+
+        try {
+            cn = new Conexion();
+            Statement st = cn.conectar().createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM tbl_sedes"
+                    + " WHERE id_sede=" + sede + " LIMIT 1");
+
+            while (rs.next()) {
+                listaDeUnaSede.add(new SedeDto(
+                        rs.getInt("id_sede"),
+                        rs.getString("nombre"),
+                        rs.getString("direccion"),
+                        rs.getString("telefono"),
+                        rs.getString("aula")
+                ));
+            }
+
+            rs.close();
+            cn.conectar().close();
+            return listaDeUnaSede;
+
+        } catch (SQLException e) {
+            System.out.println(e);
+
+        }
+        return null;
+    }
+
+    public boolean actualizarSede(SedeDto sedeDto) {
+        try {
+            cn = new Conexion();
+            PreparedStatement ps = cn.conectar().prepareStatement("UPDATE `tbl_sedes`"
+                    + " SET `id_sede`='" + sedeDto.getId_sede() + "',"
+                    + "`nombre`='" + sedeDto.getNombre() + "',"
+                    + "`direccion`='" + sedeDto.getDireccion() + "',"
+                    + "`telefono`='" + sedeDto.getTelefono() + "',"
+                    + "`aula`='" + sedeDto.getAula() + "'"
+                    + " WHERE id_sede=" + sedeDto.getId_sede());
+
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return false;
+    }
+
+    public boolean eliminarSede(int sede) {
+        try {
+            cn = new Conexion();
+            PreparedStatement ps = cn.conectar().prepareStatement("DELETE * "
+                    + "FROM tbl_sedes WHERE id_sede" + sede);
+
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return false;
+    }
+
 }
