08d13ed48fdf77e182ace49bb88b90d2be372e62
Andres Torres <andres032303@gmail.com>
2021-09-02 10:18:39 -0500

    too many resuelto

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -6,21 +6,20 @@ import java.sql.SQLException;
 
 public class Conexion {
     
-    private final String url = "jdbc:mysql://localhost:3306/dbcometohome";
+    private final String url = "//localhost:3306/dbcometohome";
     private final String user = "root";
     private final String pass = "";
     
     public Connection conectar(){
-       Connection conexion = null;
        
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conexion = DriverManager.getConnection(url, user, pass);
+            Connection conexion = DriverManager.getConnection("jdbc:mysql:" + url, user, pass);  
             return conexion;
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("error al conectar con mysql " + e.toString());
         }
-        return conexion;
+        return null;
     }
     
 }
